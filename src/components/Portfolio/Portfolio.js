27670c4eb960d02930cf6e1c210315a30a25342e
m-marc <marchenkovmaxim@gmail.com>
2021-01-22 21:11:10 +0300

    Component for portfolio items

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,18 @@
 import React from "react";
+import common from "../../common/styles/container.module.css"
+import PortfolioItem from "../PortfolioItem/PortfolioItem";
 
 const Portfolio = () => {
     return (
         <div>
-            <h2>Portfolio</h2>
-            {/*list of projects*/}
+            <div className={common.container}>
+                <h2>Portfolio</h2>
+                {/*list of projects*/}
+                <PortfolioItem title={"Futuredial.com"} link={"https://www.futuredial.com/"}
+                               img={"https://www.futuredial.com/wp-content/uploads/2019/10/futuredial-solutions-test-radi.jpg"}
+                               desc={"lorem ipsum"}
+                />
+            </div>
         </div>
     )
 }
---
+++ src/components/PortfolioItem/PortfolioItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import common from "../../common/styles/container.module.css"
+import s from "./PortfolioItem.module.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLink} from "@fortawesome/free-solid-svg-icons";
+
+
+const PortfolioItem = props => {
+    const {title, desc, img, link} = props
+    return (
+        <div className={s.wrapper}>
+            <div className={s.backgroundImg} style={{backgroundImage: `url(${img})`}}>
+                <a href={link} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLink} /></a>
+            </div>
+            <h5 className={s.title}>{title}</h5>
+            <p className={s.desc}>{desc}</p>
+        </div>
+    )
+}
+
+export default PortfolioItem
