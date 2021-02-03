32339bf949c4112f5257d9e82ea0dba888d0c2cd
m-marc <marchenkovmaxim@gmail.com>
2021-02-04 02:38:10 +0300

    styled navigation && basic styles

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,19 +5,19 @@ import {NavLink} from "react-router-dom"
 const Nav = () => {
     return (
         <nav className={s.nav}>
-            <NavLink to="/">
+            <NavLink to="/" className={s.link} exact activeClassName={s.active}>
                 <span>Home</span>
             </NavLink>
-            <NavLink to="/resume">
+            <NavLink to="/resume" className={s.link} activeClassName={s.active}>
                 <span>Resume</span>
             </NavLink>
-            <NavLink to="/portfolio">
+            <NavLink to="/portfolio" className={s.link} activeClassName={s.active}>
                 <span>Portfolio</span>
             </NavLink>
-            <NavLink to="/blog">
+            <NavLink to="/blog" className={s.link} activeClassName={s.active}>
                 <span>Blog</span>
             </NavLink>
-            <NavLink to="/contact">
+            <NavLink to="/contact" className={s.link} activeClassName={s.active}>
                 <span>Contact</span>
             </NavLink>
         </nav>
