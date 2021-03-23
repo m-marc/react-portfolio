dba842f69629c8ffaf5a09f1bdd842db7f141a95
m-marc <marchenkovmaxim@gmail.com>
2021-03-23 21:14:28 +0300

    Added particles, typing effect & profile pic

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import Header from "./components/Header/Header";
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import {Routes} from "./components/Routes/Routes";
+import Particles from "react-particles-js";
+
+const particlesOpt = {
+    "particles": {
+        "number": {
+            "value": 150,
+            "density": {
+                "enable": true,
+                "value_area":800
+            }
+        },
+        "move": {
+            "speed": 1
+        },
+    }
+}
 
 function App() {
     return (
         <BrowserRouter>
+            <Particles className="particles" params={particlesOpt}/>
             <div className="wrapper">
                 <Header />
                 <Routes />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Social from "../../components/Social/Social";
+import me from "../../img/profile.jpg";
+import TypingEffect from "react-typing-effect";
 
 const Home = () => {
     return (
         <div className={`container`}>
             <div className={`d-flex-col home_col glass glass__dark`}>
-                <h1>Hi, I am <strong className={"color-theme"}>Max Marchenkov</strong></h1>
-                <p>A frontend developer.</p>
+                <img src={me} alt="this is me" width={150} className="avatar"/>
+                <h1><strong className={"color-theme"}>Max Marchenkov</strong></h1>
+                <TypingEffect
+                    text={["I'm a Front-End developer."]}
+                    speed={200}
+                    typingDelay={700}/>
                 <Social />
             </div>
         </div>
