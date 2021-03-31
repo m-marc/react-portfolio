efa9aacadb61dda75c873e716c9afaa501369fde
m-marc <marchenkovmaxim@gmail.com>
2021-03-31 19:22:55 +0300

    header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,28 @@
-import React from "react";
-import s from "./Header.module.css"
+import React, {useState} from "react";
 import Nav from "../Nav/Nav";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Header = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    const menuToggle = () => {setIsVisible(!isVisible)}
     const today = new Date()
     return (
-        <header className={s.header}>
-            {/*logo container*/}
-            <><a href=""><img src="" alt=""/></a></>
-            <Nav />
+        <header className={`header ${isVisible ? "is-visible" : ''}`}>
+            <div className={`header-inner`}>
+                <button className="header-toggle" onClick={menuToggle}>
+                    {isVisible
+                        ? <FontAwesomeIcon icon={faTimes} size="2x" color="white"/>
+                        : <FontAwesomeIcon icon={faBars} size="2x" color="white"/>
+                    }
+
+
+                </button>
+                <Nav />
+            </div>
             {/*copyright*/}
-            <><p>&copy; {today.getFullYear()}</p></>
+            <p>&copy; {today.getFullYear()}</p>
         </header>
     )
 }
