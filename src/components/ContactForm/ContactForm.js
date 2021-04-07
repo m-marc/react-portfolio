b0ac006a95a02fd52ad13c3418f971985632bdfd
m-marc <marchenkovmaxim@gmail.com>
2021-04-07 10:38:27 +0300

    form validation

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,18 +5,18 @@ import * as yup from 'yup'
 
 const validationSchema = yup.object().shape({
     name: yup.string()
-        .min(2, 'Too short!')
-        .max(20, 'Too long!')
-        .required('Required'),
+        .min(2, 'Name is too short!')
+        .max(20, 'Name is too long!')
+        .required('Name is required'),
     subject: yup.string()
-        .min(3, 'Too short!')
-        .max(50, 'Too long!')
-        .required('Required'),
-    email: yup.string().email('Invalid email').required('Required'),
+        .min(3, 'Subject is too short!')
+        .max(50, 'Subject is too long!')
+        .required('Subject is required'),
+    email: yup.string().email('Invalid email').required('Email is required'),
     message: yup.string()
-        .min(3, 'Too short!')
-        .max(400, 'Too long!')
-        .required('Required')
+        .min(3, 'Message is too short!')
+        .max(400, 'Message is too long!')
+        .required('Message is required')
 })
 
 
@@ -33,22 +33,18 @@ export const ContactForm = () => {
         onSubmit={(data, {setSubmitting, resetForm}) => {
             setSubmitting(true);
             //async stuff here
-            console.log(data)
             setSubmitting(false)
             resetForm()
         }}
     >
-        {({values, isSubmitting, errors, touched}) => (
+        {({isSubmitting, errors}) => (
             <Form className="contact-form glass">
-                <Field placeholder={"Enter your name"} name="name" type="input" required as={TextField}/>
-                <Field placeholder="Enter your email" name="email" type="input" required as={TextField}/>
+                <Field placeholder="Enter your name" name="name" type="input" required as={TextField}/>
+                <Field placeholder="Enter your email" name="email" type="email" required as={TextField}/>
                 <Field placeholder="Enter your subject" name="subject" type="input" required as={TextField}/>
                 <Field placeholder="Enter your message" name="message" type="textarea" required cols="30" rows="6" as={TextField}/>
-                <div className="form-errors">
-                    {Object.keys(errors).length > 0 ? `Some error happened`: null}
-                </div>
                 <div className={"form-field"}>
-                    <button disabled={isSubmitting} type="submit" className={"btn"}>
+                    <button disabled={isSubmitting || !!errors} type="submit" className={"btn"}>
                         Send Mail
                     </button>
                 </div>
--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {useField} from "formik";
 
-export const TextField = ({field, form, ...props}) => {
+export const TextField = ({form, ...props}) => {
+    const [field, meta] = useField(props);
     return <div className={"form-field"}>
         {props.type === 'textarea'
             ? <textarea className={"textarea"} {...field} {...props}> </textarea>
             : <input className={"input"} {...field} {...props}/>}
+        <span className={"form-field-error"}>{meta.error && meta.touched ? meta.error : null}</span>
     </div>
 }
